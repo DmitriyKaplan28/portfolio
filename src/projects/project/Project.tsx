8bcd223da53e0b4761f8f72a24bb2555c2c101d2
DmitriyKaplan28 <dsk007@mail.ru>
2022-06-17 22:08:49 +0300

    Projects

---
+++ src/projects/project/Project.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import style from './Project.module.css';
+
+type ProjectPropsType = {
+    title: string
+    description: string
+}
+
+const Project = (props: ProjectPropsType) => {
+    return (
+        <div className={style.project}>
+            <div className={style.icon}><button className={style.button}>Take a look</button></div>
+            <div>
+                <h3 className={style.title}>{props.title}</h3>
+            <span className={style.description}>{props.description}</span>
+            </div>
+        </div>
+    );
+};
+
+export default Project;
