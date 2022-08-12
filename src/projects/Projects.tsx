2662eafffda087efee461130c5a96e44abb000e7
DmitriyKaplan28 <dsk007@mail.ru>
2022-08-12 17:48:33 +0300

    1:27:50

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import style from './Projects.module.css';
 import styleContainer from '../common/styles/Container.module.css';
 import Project from "./project/Project";
+import {Title} from "../common/components/title/Title";
 
 export const Projects = () => {
     return (
         <div className={style.projectsBlock}>
             <div className={`${styleContainer.container} ${style.projectsContainer}`}>
-                <h2 className={style.title}>My projects</h2>
+                <Title title={'My projects'}/>
                 <div className={style.projects}>
-                    <Project title={'project'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, quas.'}/>
-                    <Project title={'project'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, quas.'}/>
+                    <Project title={'project'}
+                             description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, quas.'}/>
+                    <Project title={'project'}
+                             description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, quas.'}/>
                 </div>
             </div>
-
         </div>
     );
 };
--- src/remote-work/RemoteWork.tsx
+++ src/remote-work/RemoteWork.tsx
@@ -7,7 +7,7 @@ export const RemoteWork = () => {
     return (
         <div className={style.remoteBlock}>
             <div className={`${styleContainer.container} ${style.remoteContainer}`}>
-                <h2 className={style.title}>I an looking for remote work options</h2>
+                <h2 className={style.title}>I am looking for remote work options</h2>
                 <div className={style.buttonContainer}>
                     <button>Hire me</button>
                 </div>
