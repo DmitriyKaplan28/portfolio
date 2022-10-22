24ab411c7ec250a85ab5065e80eaf08c4fd2a427
DmitriyKaplan28 <dsk007@mail.ru>
2022-10-23 00:26:04 +0300

    feat: add particles library

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -2,6 +2,20 @@ import React from 'react';
 import style from './Main.module.scss';
 import styleContainer from '../common/styles/Container.module.css';
 import myPhotoImage from "../assets/images/my-photo.png";
+import Particles from "react-tsparticles";
+
+const particlesOpt = {
+    "particles": {
+        "number": {
+            "value": 150,
+            "density": {
+                "enable": true,
+                "value_area": 800,
+            }
+        }
+    }
+}
+
 
 export const Main = () => {
 
@@ -11,6 +25,7 @@ export const Main = () => {
 
     return (
         <div className={style.mainBlock}>
+            <Particles className={style.particles} params={particlesOpt} />
             <div className={styleContainer.container}>
                 <div className={style.greetings}>
                     <span className={style.text}>Hi there</span>
