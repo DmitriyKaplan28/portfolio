81ecd13e4fec81f058a0e1a88c8c5cc6d63da375
DmitriyKaplan28 <dsk007@mail.ru>
2023-08-05 12:44:36 +0300

    refactor: destructuring Project.tsx

--- src/projects/project/Project.tsx
+++ src/projects/project/Project.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import style from './Project.module.scss';
+import s from './Project.module.scss';
 
 type styleType = {
     backgroundImage: string
@@ -11,15 +11,15 @@ type ProjectPropsType = {
     style: styleType
 }
 
-const Project = (props: ProjectPropsType) => {
+const Project = ({title, description, style}: ProjectPropsType) => {
     return (
-        <div className={style.project}>
-            <div className={style.icon} style={props.style}>
-                <button className={style.button}>Take a look</button>
+        <div className={s.project}>
+            <div className={s.icon} style={style}>
+                <button className={s.button}>Take a look</button>
             </div>
-            <div className={style.projectInfo}>
-                <h3 className={style.projectTitle}>{props.title}</h3>
-                <span className={style.description}>{props.description}</span>
+            <div className={s.projectInfo}>
+                <h3 className={s.projectTitle}>{title}</h3>
+                <span className={s.description}>{description}</span>
             </div>
         </div>
     );
